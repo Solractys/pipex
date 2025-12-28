d509d12e8d7c2d8a2e990bcb0545b6f756a27710
Solractys <carloseduardosilvasouzadelima@gmail.com>
2025-12-28 20:56:30 -0300

    fix fd issue

--- includes/pipex.h
+++ includes/pipex.h
@@ -7,5 +7,17 @@
 #include <fcntl.h>
 #include "../includes/ft_printf/ft_printf.h"
 
+typedef struct	
+{
+	char	**cmd1;
+	char	**cmd2;
+}	t_command;
+
+typedef struct
+{
+	char	*line1;
+	char	*line2;
+	char	**path;
+}	t_info;
 
 # endif
--- src/main.c
+++ src/main.c
@@ -6,16 +6,12 @@
 /*   By: csilva-s <csilva-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/17 14:53:11 by csilva-s          #+#    #+#             */
-/*   Updated: 2025/12/27 20:19:02 by csilva-s         ###   ########.fr       */
+/*   Updated: 2025/12/28 20:50:15 by csilva-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
 
-int	check_path(char *str)
-{
-	return (ft_strncmp(str, "PATH=", 5) == 0);
-}
 char	**find_path(char **envp)
 {
 	char **path;
@@ -25,7 +21,7 @@ char	**find_path(char **envp)
 	path = NULL;
 	while (envp[i] != NULL)
 	{
-		if (check_path(envp[i]))
+		if (ft_strncmp(envp[i], "PATH=", 5) == 0)
 		{
 			path = ft_split(envp[i] + 5, ':');
 			break ;
@@ -72,17 +68,14 @@ void	free_path(char **path)
 
 int	main(int argc, char **argv, char **envp)
 {
-	if (argc != 5)
-		return (0);
 	int	fd[2];
 	int infile;
 	int	outfile;
-	char **cmd1;
-	char **cmd2;
-	char **path;
-	char	*line1;
-	char	*line2;
+	t_command	commands;
+	t_info	info_var;
 
+	if (argc != 5)
+		return (0);
 	infile = open(argv[1], O_RDONLY);
 	if (infile < 0)
 		exit(1);
@@ -92,40 +85,40 @@ int	main(int argc, char **argv, char **envp)
 		close(infile);
 		exit(1);
 	}
-	cmd1 = ft_split(argv[2], ' ');
-	cmd2 = ft_split(argv[3], ' ');
-	path = find_path(envp);
-	if (path == NULL)
+	commands.cmd1 = ft_split(argv[2], ' ');
+	commands.cmd2 = ft_split(argv[3], ' ');
+	info_var.path = find_path(envp);
+	if (info_var.path == NULL)
 	{
-		free_path(cmd1);
-		free_path(cmd2);
+		free_path(commands.cmd1);
+		free_path(commands.cmd2);
 		ft_putstr_fd("Error to clone path", 2);
 		exit(1);
 	}
-	line1 = find_correct_path(path, cmd1[0]);
-	if (line1 == NULL)
+	info_var.line1 = find_correct_path(info_var.path, commands.cmd1[0]);
+	if (info_var.line1 == NULL)
 	{
 		ft_putstr_fd("this file don't exits", 2);
-		free_path(path);
-		free_path(cmd1);
-		free_path(cmd2);
+		free_path(info_var.path);
+		free_path(commands.cmd1);
+		free_path(commands.cmd2);
 		close (infile);
 		close (outfile);
 		exit(1);
 	}
-	line2 = find_correct_path(path, cmd2[0]);
-	if (line2 == NULL)
+	info_var.line2 = find_correct_path(info_var.path, commands.cmd2[0]);
+	if (info_var.line2 == NULL)
 	{
 		ft_putstr_fd("this file don't exits", 2);
-		free_path(path);
-		free(line1);
-		free_path(cmd1);
-		free_path(cmd2);
+		free_path(info_var.path);
+		free(info_var.line1);
+		free_path(commands.cmd1);
+		free_path(commands.cmd2);
 		close (infile);
 		close (outfile);
 		exit (1);
 	}
-	free_path(path);
+	free_path(info_var.path);
 	pipe(fd);
 	if (fork() == 0)
 	{
@@ -135,14 +128,11 @@ int	main(int argc, char **argv, char **envp)
 		close(fd[1]);
 		close(infile);
 		close(outfile);
-		execve(line1, cmd1, envp);
-		free(line1);
-		free(line2);
-		free_path(cmd1);
-		free_path(cmd2);
+		execve(info_var.line1, commands.cmd1, envp);
 		ft_putstr_fd("command 1 error", 2);
 		exit(1);
 	}
+	close(fd[1]);
 	if (fork() == 0)
 	{
 		dup2(outfile, 1);
@@ -151,23 +141,19 @@ int	main(int argc, char **argv, char **envp)
 		close(fd[1]);
 		close(infile);
 		close(outfile);
-		execve(line2, cmd2, envp);
-		free(line1);
-		free(line2);
-		free_path(cmd1);
-		free_path(cmd2);
+		execve(info_var.line2, commands.cmd2, envp);
 		ft_putstr_fd("command 2 error", 2);
 		exit(1);
 	}
-	free_path(cmd1);
-	free_path(cmd2);
-	free(line1);
-	free(line2);
 	close(fd[0]);
 	close(fd[1]);
 	close(infile);
 	close(outfile);
 	wait(NULL);
 	wait(NULL);
+	free_path(commands.cmd1);
+	free_path(commands.cmd2);
+	free(info_var.line1);
+	free(info_var.line2);
 	return (0);
 }
