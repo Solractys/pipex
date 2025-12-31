5ff25c8e94917d8cac95f9da6b80a9b7fbd0e670
nanashi <carloseduardosilvasouzadelima@gmail.com>
2025-12-30 21:52:26 -0300

    teste

--- commands.gdb
+++ commands.gdb
@@ -1,7 +1,6 @@
 set follow-fork-mode child
 set detach-on-fork off
 b main
-r infile "grep a" "wc -l" outfile
+r infile "cat" "grep hello" "wc -l" outfile
 set logging enable on
-backtrace
 info locals
--- src/main.c
+++ src/main.c
@@ -39,27 +39,93 @@ t_info	init_pipex(char **av, char **envp)
 	return (pipex);
 }
 
-int	main(int argc, char **argv, char **envp)
+// int	main(int argc, char **argv, char **envp)
+// {
+// 	t_info	pipex;
+// 	int		fd[2];
+// 	int		pid;
+// 	int		pid2;
+//
+// 	if (argc != 5)
+// 		return (0);
+// 	pipex = init_pipex(argv, envp);
+// 	pipe(fd);
+// 	pid = fork();
+// 	if (pid == 0)
+// 		execute_routine(pipex, fd, 1, envp);
+// 	close(fd[1]);
+// 	pid2 = fork();
+// 	if (pid2 == 0)
+// 		execute_routine(pipex, fd, 2, envp);
+// 	close(fd[0]);
+// 	free_pipex(pipex);
+// 	wait(NULL);
+// 	wait(NULL);
+// 	return (0);
+// }
+int	main(int ac, char **av, char **envp)
 {
-	t_info	pipex;
-	int		fd[2];
-	int		pid;
-	int		pid2;
+	int	fd[2];
+	int	cmd_count;
+	int	init_cmd;
+	int	infile;
+	int	outfile;
+	int	i;
+	int	pid;
+	int	old_fd;
 
-	if (argc != 5)
+	if (ac < 5)
 		return (0);
-	pipex = init_pipex(argv, envp);
-	pipe(fd);
-	pid = fork();
-	if (pid == 0)
-		execute_routine(pipex, fd, 1, envp);
-	close(fd[1]);
-	pid2 = fork();
-	if (pid2 == 0)
-		execute_routine(pipex, fd, 2, envp);
+	infile = open(av[1], O_RDONLY);
+	outfile = open(av[ac - 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	cmd_count = ac - 3;
+	init_cmd = 2;
+	old_fd = -1;
+	i = 0;
+	while (i < cmd_count)
+	{
+		char	**current_cmd = ft_split(av[init_cmd + i], ' ');
+		ft_printf("%s", current_cmd[0]);
+		if (i < cmd_count - 1)
+			pipe(fd);
+		pid = fork();
+		if (pid == 0)
+		{
+			if (old_fd == -1)
+				old_fd = infile;
+			else
+				dup2(old_fd, STDIN_FILENO);
+			if (i == cmd_count - 1)
+				dup2(outfile, STDOUT_FILENO);
+			else
+				dup2(fd[1], STDOUT_FILENO);
+			close(fd[0]);
+			close(fd[1]);
+			close(infile);
+			close(outfile);
+			char	**path = ft_split(find_path(envp), ':');
+			char	*cmd_path = find_line(path, current_cmd[0]);
+			if (!cmd_path)
+			{
+				perror("command not found");
+				exit(1);
+			}
+			execve(cmd_path, current_cmd, envp);
+			perror("failed");
+			exit(1);
+		}
+		close(fd[1]);
+		close(old_fd);
+		old_fd = fd[0];
+		i++;
+	}
+	while(i-- > 0)
+	{
+		ft_printf("o caraio");
+		wait(NULL);
+	}
+	close(infile);
+	close(outfile);
 	close(fd[0]);
-	free_pipex(pipex);
-	wait(NULL);
-	wait(NULL);
-	return (0);
+	close(fd[1]);
 }
